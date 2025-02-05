8ca6310b2fb150e6aded973362d1fa8111301914
胡楚衿 <hcj@shangy.com>
2025-02-05 11:49:14 +0800

    change

--- src/components/chat/chatBubble/YoutubePlayer.vue
+++ src/components/chat/chatBubble/YoutubePlayer.vue
@@ -1,54 +1,36 @@
-<script setup>
-import {computed} from "vue";
-import {useConfigStore} from "@/stores/useConfigStore.js";
-import {useChatStore} from "@/stores/useChatStore.js";
-
-const configStore = useConfigStore();
-const chatStore = useChatStore();
-
-const props = defineProps(['message', 'index']);
-
-const index = computed(() => {
-  return props.index;
-});
-
-const message = computed(() => {
-  return props.message;
-});
-
-function showMoreFunctionsModal(index) {
-  if (configStore.isLoading) {
-    return
-  }
-
-  chatStore.showMoreFunctionsModal(index);
-}
-
-</script>
-
 <template>
-  <div :class="['chat-message-bubble-canvas', message.role === 'user' ? 'chat-message-bubble-canvas-right' : 'chat-message-bubble-canvas-left']" >
-    <span :class="[configStore.fontSize === 0 ? 'text-size-normal' : 'text-size-big']" v-html="message.content"></span>
-    <p :style="['text-align: right']" v-if="configStore.showMoreButton">
-      <a @click="showMoreFunctionsModal(index)" style="color: black"><i class="bi bi-three-dots"></i></a>
-    </p>
+  <div class="youtube-player">
+    <iframe
+      :src="videoUrl"
+      width="100%"
+      height="315"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
   </div>
 </template>
 
+<script>
+export default {
+  props: {
+    videoId: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    videoUrl() {
+      return `https://www.youtube.com/embed/${this.videoId}`;
+    },
+  },
+};
+</script>
+
 <style scoped>
-.chat-message-bubble-canvas {
-  background: #E4E4E7;
-  max-width: 80%;
-  padding: 10px;
-  border-radius: 10px;
-}
-
-.chat-message-bubble-canvas-left {
-  float: left;
-}
-
-.chat-message-bubble-canvas-right {
-  float: right;
-  background: #E2EAFD;
+.youtube-player {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
